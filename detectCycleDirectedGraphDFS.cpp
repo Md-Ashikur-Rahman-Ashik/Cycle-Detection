ce2d5ab999d33a0785331bf1eaaa973c1650359f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-08-28 12:29:31 +0600

    Detected cycle in directed graph

--- detectCycleDirectedGraphDFS.cpp
+++ detectCycleDirectedGraphDFS.cpp
@@ -6,8 +6,23 @@ bool visitedArray[105];
 bool pathVisited[105];
 bool detectCycle;
 
-void dfs(int sourceNode){
+void dfs(int sourceNode)
+{
     visitedArray[sourceNode] = true;
+    pathVisited[sourceNode] = true;
+
+    for (int childNode : adjacencyList[sourceNode])
+    {
+        if(visitedArray[childNode] == true && pathVisited[childNode] == true){
+            detectCycle = true;
+        }
+
+        if (visitedArray[childNode] == false)
+        {
+            dfs(childNode);
+            pathVisited[childNode] = false;
+        }
+    }
 }
 
 int main()
@@ -28,12 +43,24 @@ int main()
 
     for (int i = 0; i < numberOfNode; i++)
     {
-        if(visitedArray[i] == false){
+        if (visitedArray[i] == false)
+        {
             dfs(i);
         }
     }
-    
-    
+
+    if(detectCycle){
+        cout << "Cycle Detected" << endl;
+    } else{
+        cout << "Cycle not detected" << endl;
+    }
 
     return 0;
 }
+
+// Input
+// 4 4
+// 0 1
+// 1 2
+// 2 3
+// 3 0
